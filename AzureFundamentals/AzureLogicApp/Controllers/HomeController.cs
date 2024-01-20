16a470106465a09866192cac834b8e95c4ebf11b
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-01-20 09:01:07 +0300

    Azure Logic Apps Workshop
    Inserting a row to SQL Server, sending an email, uploading a blob and copying blobs to another container using logic apps triggers/actions

---
+++ AzureFundamentals/AzureLogicApp/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using AzureLogicAppWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+
+namespace AzureLogicAppWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        static readonly HttpClient client = new HttpClient();
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public HomeController(ILogger<HomeController> logger, BlobServiceClient blobServiceClient)
+        {
+            _logger = logger;
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(SpookyRequest request, IFormFile file)
+        {
+            request.Id = Guid.NewGuid().ToString();
+            var jsonContent = JsonConvert.SerializeObject(request);
+
+            using var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("https://prod-39.eastus.logic.azure.com:443/workflows/ad8baa1b80e94bc985ae5356f950509a/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vTtdG0vgsSvYIFrfuGM0D5ggN-oCWo0W9s4JY-_yE7M", content);
+
+            if (file != null)
+            {
+                var fileName = request.Id + Path.GetExtension(file.FileName);
+                var blobContainerClient = _blobServiceClient.GetBlobContainerClient("logicappholder");
+                var blobClient = blobContainerClient.GetBlobClient(fileName);
+
+                var httpHeaders = new BlobHttpHeaders()
+                {
+                    ContentType = file.ContentType
+                };
+
+                await blobClient.UploadAsync(file.OpenReadStream(), httpHeaders);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
