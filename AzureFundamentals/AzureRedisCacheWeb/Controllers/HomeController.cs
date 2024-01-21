15b2409454b6db9bfce2f04b2d47ddf055e6c2a2
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-01-21 10:41:46 +0300

    Azure for Redis Workshop

---
+++ AzureFundamentals/AzureRedisCacheWeb/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using AzureRedisCacheWeb.Data;
+using AzureRedisCacheWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace AzureRedisCacheWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IDistributedCache _cache;
+
+        public HomeController(
+            ILogger<HomeController> logger, 
+            ApplicationDbContext dbContext, 
+            IDistributedCache cache)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+            _cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            //_cache.Remove("categoryList");
+            //_cache.Refresh("categoryList");
+
+            List<Category> categoryList = new();
+            var cachedCategories = _cache.GetString("categoryList");
+
+            if (!string.IsNullOrEmpty(cachedCategories))
+            {
+                categoryList = JsonConvert.DeserializeObject<List<Category>>(cachedCategories);
+            }
+            else
+            {
+                categoryList = _dbContext.Categories.ToList();
+
+                //Caching
+                DistributedCacheEntryOptions cacheOptions = new()
+                {
+                    AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(30),
+                };
+
+                _cache.SetString("categoryList", JsonConvert.SerializeObject(categoryList), cacheOptions);
+            }
+
+            return View(categoryList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AzureFundamentals/AzureRedisCacheWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AzureRedisCacheWeb.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureRedisCacheWeb.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
